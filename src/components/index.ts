1a441aa3f996fb160f5e00eca833ebc2b9b54094
Red <red.pace.dev@gmail.com>
2025-09-19 23:23:33 +0200

    feat: update component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export { default as ImageCard } from './ImageCard';
-export { default as ImageGallery } from './ImageGallery';
+export * from './primitives';
+export * from './features';
+export * from './pages';
